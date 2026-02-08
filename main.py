05bff49e1a84063562ce10f1d07bac039ddc710a
birisivarabi-jpg <birisivarabi@gmail.com>
2026-02-08 19:19:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+print("BOT BASLADI")
+
+import telebot
+from telebot import types
+import threading
+import time
+from datetime import datetime
+
+
+
+TOKEN = "8305501223:AAFRNXBEdFSVY7g8_9i7c5-m15krH0mbaTM"
+bot = telebot.TeleBot(TOKEN)
+
 
